997beb56fc907df53f9e78911bd8c923323e963a
house612e4 <house612e4@gmail.com>
2026-04-24 03:11:12 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,15 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// তোমার Firebase Console থেকে পাওয়া Config
 const firebaseConfig = {
   apiKey: "AIzaSyA9oKyU5LVspCQNtEbuPYsXe9yzvm63A8U",
   authDomain: "lillahi-etimkhana.firebaseapp.com",
   projectId: "lillahi-etimkhana",
   storageBucket: "lillahi-etimkhana.firebasestorage.app",
-  messagingSenderId: "777373297360",
-  appId: "1:777373297360:web:61c04a82a1098204ea8cd5",
+  messagingSenderId: "777737297360",
+  appId: "1:777737297360:web:61c04a82a1098204ea8cd5",
   measurementId: "G-H8ZBFHJE6M"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
